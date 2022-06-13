d21654ab97f0c6f918579562cb4979825bf86959
Hasibul Hasan Chowdhury <hchowdhury404@gmail.com>
2022-06-14 01:42:20 +0600

    restructure files

--- app/views.py
+++ app/views.py
@@ -1,25 +1,7 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-import os
+from flask import request, jsonify
 
-app = Flask(__name__)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-db = SQLAlchemy(app)
-
-class Item(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(80), unique=True, nullable=False)
-  content = db.Column(db.String(120), unique=True, nullable=False)
-
-  def __init__(self, title, content):
-    self.title = title
-    self.content = content
-
-db.create_all()
+from app.app import app, db
+from app.model import Item
 
 @app.route('/items/<id>', methods=['GET'])
 def get_item(id):
